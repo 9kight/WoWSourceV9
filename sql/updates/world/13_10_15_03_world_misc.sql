efa5ee63a2be86167726af194ee6bd3d2b3abfb9
Stalker <geany_100@yahoo.com>
2013-10-15 12:17:01 +0300

    Create 13_10_15_03_world_misc.sql
    The Black Morass mobs spells ability and skinning

---
+++ sql/updates/world/13_10_15_03_world_misc.sql
@@ -0,0 +1,56 @@
+UPDATE `creature_template` SET spell1='31550', spell2='40504', spell3='37853', spell4='37605', spell5='39049', spell6='31473', spell7='31422' WHERE entry='17881';
+DELETE FROM `skinning_loot_template` WHERE entry='17881';
+INSERT INTO `skinning_loot_template` VALUES ('17881', '21887', '90.23', '1', '0', '1', '9');
+UPDATE `creature_template` SET skinloot='17881' WHERE entry='17881';
+
+UPDATE `creature_template` SET spell1='31457', spell2='38538', spell3='38539', spell4='31472', spell5='38540', spell6='31550', spell7='31467' WHERE entry='17879';
+DELETE FROM `skinning_loot_template` WHERE entry='17879';
+INSERT INTO `skinning_loot_template` VALUES ('17879', '21887', '90.36', '1', '0', '1', '7');
+UPDATE `creature_template` SET skinloot='17879' WHERE entry='17879';
+
+UPDATE `creature_template` SET spell1='31550', spell2='55095', spell3='31458', spell4='31464', spell5='31589', spell6='38592', spell7='38593', spell8='31475' WHERE entry='17880';
+DELETE FROM `skinning_loot_template` WHERE entry='17880';
+INSERT INTO `skinning_loot_template` VALUES ('17880', '21887', '90.16', '1', '0', '1', '9');
+UPDATE `creature_template` SET skinloot='17880' WHERE entry='17880';
+
+UPDATE `creature_template` SET spell1='34366' WHERE entry='18983';
+
+UPDATE `creature_template` SET spell1='34370' WHERE entry='17952';
+DELETE FROM `skinning_loot_template` WHERE entry='17952';
+INSERT INTO `skinning_loot_template` VALUES ('17952', '21887', '80.85', '1', '0', '1', '6');
+INSERT INTO `skinning_loot_template` VALUES ('17952', '25649', '9.38', '1', '0', '1', '8');
+UPDATE `creature_template` SET skinloot='17952' WHERE entry='17952';
+
+UPDATE `creature_template` SET spell1='15657', spell2='7159', spell3='31326', spell4='38520', spell5='30832' WHERE entry='17835';
+
+UPDATE `creature_template` SET spell1='38538', spell2='38539', spell3='38540', spell4='31467' WHERE entry='21697';
+DELETE FROM `skinning_loot_template` WHERE entry='21697';
+INSERT INTO `skinning_loot_template` VALUES ('21697', '21887', '105.88', '1', '0', '1', '4');
+UPDATE `creature_template` SET skinloot='21697' WHERE entry='21697';
+
+UPDATE `creature_template` SET spell1='15124', spell2='15230', spell3='33860', spell4='33623', spell5='31326' WHERE entry='17892';
+
+UPDATE `creature_template` SET spell1='31326', spell2='37321', spell3='17198', spell4='15580', spell5='34920' WHERE entry='18994';
+
+UPDATE `creature_template` SET spell1='31458', spell2='31464', spell3='38592', spell4='38593' WHERE entry='21698';
+
+UPDATE `creature_template` SET spell1='31326', spell2='38526', spell3='13341', spell4='36807', spell5='15241' WHERE entry='18995';
+
+UPDATE `creature_template` SET spell1='31326' WHERE entry='21818';
+
+UPDATE `creature_template` SET spell1='36276', spell2='12542', spell3='8269', spell4='36275', spell5='38533' WHERE entry='21104';
+DELETE FROM `skinning_loot_template` WHERE entry='21104';
+INSERT INTO `skinning_loot_template` VALUES ('21104', '21887', '90.41', '1', '0', '1', '8');
+UPDATE `creature_template` SET skinloot='21104' WHERE entry='21104';
+
+UPDATE `creature_template` SET spell1='8269', spell2='9080', spell3='11428', spell4='15708', spell5='35054' WHERE entry='17839';
+
+UPDATE `creature_template` SET spell1='34975' WHERE entry='20201';
+
+UPDATE `creature_template` SET spell1='22766' WHERE entry='18982';
+DELETE FROM `skinning_loot_template` WHERE entry='18982';
+INSERT INTO `skinning_loot_template` VALUES ('18982', '21887', '80.61', '1', '0', '1', '6');
+INSERT INTO `skinning_loot_template` VALUES ('18982', '25649', '9.28', '1', '0', '1', '8');
+UPDATE `creature_template` SET skinloot='18982' WHERE entry='18982';
+
+UPDATE `creature_template` SET spell1='31478' WHERE entry='17918';
