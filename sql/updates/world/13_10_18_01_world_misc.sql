c42850276467d2c088b59f46c0546cbe79fd1175
Stalker <geany_100@yahoo.com>
2013-10-18 09:47:38 +0300

    Create 13_10_18_01_world_misc.sql

---
+++ sql/updates/world/13_10_18_01_world_misc.sql
@@ -0,0 +1,113 @@
+UPDATE `creature_template` SET spell1='30917', spell2='38459', spell3='30916', spell4='30913', spell5='38458' WHERE entry='17380';
+
+UPDATE `creature_template` SET spell1='30940', spell2='30938', spell3='30935', spell4='33775', spell5='37371', spell6='40070', spell7='17228', spell8='37370' WHERE entry='17377';
+
+UPDATE `creature_template` SET spell1='30923', spell2='40059', spell3='30925' WHERE entry='17381';
+DELETE FROM `pickpocketing_loot_template` WHERE entry='17381';
+INSERT INTO `pickpocketing_loot_template` VALUES ('17381', '29569', '0.54', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17381', '29570', '0.43', '1', '0', '1', '1');
+UPDATE `creature_template` SET pickpocketloot='17381' WHERE entry='17381';
+
+UPDATE `creature_template` SET spell1='22120', spell2='8269' WHERE entry='17429';
+DELETE FROM `pickpocketing_loot_template` WHERE entry='17429';
+INSERT INTO `pickpocketing_loot_template` VALUES ('17429', '29569', '1.11', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17429', '29570', '0.92', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17429', '27854', '0.24', '1', '0', '1', '1');
+UPDATE `creature_template` SET pickpocketloot='17429' WHERE entry='17429';
+
+UPDATE `creature_template` SET spell1='58747', spell2='15615', spell3='18072' WHERE entry='17400';
+
+UPDATE `creature_template` SET spell1='15615', spell2='18072' WHERE entry='18894';
+
+UPDATE `creature_template` SET spell1='13321', spell2='30849' WHERE entry='17401';
+DELETE FROM `skinning_loot_template` WHERE entry='17401';
+INSERT INTO `skinning_loot_template` VALUES ('17401', '25649', '71.43', '1', '0', '1', '8');
+INSERT INTO `skinning_loot_template` VALUES ('17401', '21887', '15.68', '1', '0', '1', '6');
+INSERT INTO `skinning_loot_template` VALUES ('17401', '25707', '4.09', '1', '0', '1', '5');
+UPDATE `creature_template` SET skinloot='17401' WHERE entry='17401';
+
+UPDATE `creature_template` SET spell1='58506' WHERE entry='54629';
+
+UPDATE `creature_template` SET spell1='13341', spell2='11921', spell3='14034' WHERE entry='19016';
+
+UPDATE `creature_template` SET spell1='16144', spell2='15242', spell3='17290' WHERE entry='17477';
+
+UPDATE `creature_template` SET spell1='58506' WHERE entry='54746';
+
+UPDATE `creature_template` SET spell1='15655', spell2='14516' WHERE entry='17370';
+DELETE FROM `pickpocketing_loot_template` WHERE entry='17370';
+INSERT INTO `pickpocketing_loot_template` VALUES ('17370', '29569', '1.97', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17370', '29570', '1.67', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17370', '27854', '0.69', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17370', '27855', '0.63', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17370', '22829', '0.39', '1', '0', '1', '1');
+UPDATE `creature_template` SET pickpocketloot='17370' WHERE entry='17370';
+
+UPDATE `creature_template` SET spell1='8599', spell2='10966' WHERE entry='17626';
+DELETE FROM `pickpocketing_loot_template` WHERE entry='17626';
+INSERT INTO `pickpocketing_loot_template` VALUES ('17626', '29570', '0.99', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17626', '29569', '0.86', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17626', '27855', '0.48', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17626', '22829', '0.32', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17626', '27854', '0.29', '1', '0', '1', '1');
+UPDATE `creature_template` SET pickpocketloot='17626' WHERE entry='17626';
+
+UPDATE `creature_template` SET spell1='30832', spell2='34969', spell3='6434', spell4='30991' WHERE entry='17491';
+DELETE FROM `pickpocketing_loot_template` WHERE entry='17491';
+INSERT INTO `pickpocketing_loot_template` VALUES ('17491', '29569', '1.45', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17491', '29570', '1.33', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17491', '27855', '0.43', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17491', '27854', '0.39', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17491', '22829', '0.31', '1', '0', '1', '1');
+UPDATE `creature_template` SET pickpocketloot='17491' WHERE entry='17491';
+
+UPDATE `creature_template` SET spell1='81219' WHERE entry='17624';
+
+UPDATE `creature_template` SET spell1='22427', spell2='31900' WHERE entry='17398';
+DELETE FROM `pickpocketing_loot_template` WHERE entry='17398';
+INSERT INTO `pickpocketing_loot_template` VALUES ('17398', '29569', '1.24', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17398', '29570', '1.12', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17398', '27854', '0.35', '1', '0', '1', '1');
+UPDATE `creature_template` SET pickpocketloot='17398' WHERE entry='17398';
+
+UPDATE `creature_template` SET spell1='32202', spell2='31865' WHERE entry='17399';
+
+UPDATE `creature_template` SET spell1='11978', spell2='3391' WHERE entry='17397';
+DELETE FROM `pickpocketing_loot_template` WHERE entry='17397';
+INSERT INTO `pickpocketing_loot_template` VALUES ('17397', '29570', '2.78', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17397', '29569', '2.59', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17397', '27855', '1.11', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17397', '27854', '0.89', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17397', '22829', '0.73', '1', '0', '1', '1');
+UPDATE `creature_template` SET pickpocketloot='17397' WHERE entry='17397';
+
+UPDATE `creature_template` SET spell1='30937', spell2='15472', spell3='12739' WHERE entry='17653';
+
+UPDATE `creature_template` SET spell1='15242', spell2='17290', spell3='16102', spell4='18399', spell5='30851', spell6='30853' WHERE entry='17395';
+DELETE FROM `pickpocketing_loot_template` WHERE entry='17395';
+INSERT INTO `pickpocketing_loot_template` VALUES ('17395', '29569', '1.29', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17395', '29570', '1.24', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17395', '27854', '0.46', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17395', '27855', '0.37', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17395', '22829', '0.29', '1', '0', '1', '1');
+UPDATE `creature_template` SET pickpocketloot='17395' WHERE entry='17395';
+
+UPDATE `creature_template` SET spell1='6726', spell2='40064', spell3='40062', spell4='32784', spell5='30846' WHERE entry='17414';
+DELETE FROM `pickpocketing_loot_template` WHERE entry='17414';
+INSERT INTO `pickpocketing_loot_template` VALUES ('17414', '29569', '1.73', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17414', '29570', '1.58', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17414', '27855', '0.63', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17414', '27854', '0.6', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17414', '22829', '0.29', '1', '0', '1', '1');
+UPDATE `creature_template` SET pickpocketloot='17414' WHERE entry='17414';
+
+UPDATE `creature_template` SET spell1='37113', spell2='32197', spell3='13338', spell4='33111', spell5='32863', spell6='15472', spell7='12739' WHERE entry='17371';
+DELETE FROM `pickpocketing_loot_template` WHERE entry='17371';
+INSERT INTO `pickpocketing_loot_template` VALUES ('17371', '29570', '2.27', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17371', '29569', '2.18', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17371', '27854', '1.01', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17371', '27855', '0.74', '1', '0', '1', '1');
+INSERT INTO `pickpocketing_loot_template` VALUES ('17371', '22829', '0.57', '1', '0', '1', '1');
+UPDATE `creature_template` SET pickpocketloot='17371' WHERE entry='17371';
+
+UPDATE `creature_template` SET spell1='58506' WHERE entry='54607';
