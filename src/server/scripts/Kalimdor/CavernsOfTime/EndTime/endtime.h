ef9d8609b8ffd3cd0cc6ef4387b2007bb8c17fb8
heybaby2707 <heybaby2707@gmail.com>
2013-10-30 01:06:41 +0700

    Reupload Endtime Instance

---
+++ src/server/scripts/Kalimdor/CavernsOfTime/EndTime/endtime.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2008-2011 Trinity <http://www.trinitycore.org/>
+ *
+ * Copyright (C) 2012-2013 StealthCoders
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+#ifndef DEF_ENDTIME_H
+#define DEF_ENDTIME_H
+
+enum Data
+{
+    DATA_EMERALD_EVENT,
+    DATA_AZURE_EVENT,
+    DATA_OBSIDIAN_EVENT,
+};
+
+#endif
