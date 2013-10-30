35cc5a56fabfaefd8585a7722d69d7cc9116f6eb
Stalker <geany_100@yahoo.com>
2013-10-30 09:13:05 +0200

    Create world_misc_13_10_30_00.sql
    Fix elwyn forest wrong equipment of npc

---
+++ sql/updates/world/world_misc_13_10_30_00.sql
@@ -0,0 +1,101 @@
+UPDATE creature_template SET equipment_id=0 WHERE entry=118;
+UPDATE creature SET equipment_id=0 WHERE id=118;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=1949;
+UPDATE creature SET equipment_id=0 WHERE id=1949;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=524;
+UPDATE creature SET equipment_id=0 WHERE id=524;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=383;
+UPDATE creature SET equipment_id=0 WHERE id=383;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=1632;
+UPDATE creature SET equipment_id=0 WHERE id=1632;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=295;
+UPDATE creature SET equipment_id=0 WHERE id=295;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=253;
+UPDATE creature SET equipment_id=0 WHERE id=253;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=2329;
+UPDATE creature SET equipment_id=0 WHERE id=2329;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=797;
+UPDATE creature SET equipment_id=0 WHERE id=797;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=796;
+UPDATE creature SET equipment_id=0 WHERE id=796;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=795;
+UPDATE creature SET equipment_id=0 WHERE id=795;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=1975;
+UPDATE creature SET equipment_id=0 WHERE id=1975;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=278;
+UPDATE creature SET equipment_id=0 WHERE id=278;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=279;
+UPDATE creature SET equipment_id=0 WHERE id=279;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=2457;
+UPDATE creature SET equipment_id=0 WHERE id=2457;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=2456;
+UPDATE creature SET equipment_id=0 WHERE id=2456;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=2455;
+UPDATE creature SET equipment_id=0 WHERE id=2455;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=1414;
+UPDATE creature SET equipment_id=0 WHERE id=1414;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=1413;
+UPDATE creature SET equipment_id=0 WHERE id=1413;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=1415;
+UPDATE creature SET equipment_id=0 WHERE id=1415;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=2198;
+UPDATE creature SET equipment_id=0 WHERE id=2198;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=1212;
+UPDATE creature SET equipment_id=0 WHERE id=1212;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=1444;
+UPDATE creature SET equipment_id=0 WHERE id=1444;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=1646;
+UPDATE creature SET equipment_id=0 WHERE id=1646;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=1333;
+UPDATE creature SET equipment_id=0 WHERE id=1333;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=118;
+UPDATE creature SET equipment_id=0 WHERE id=118;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=1341;
+UPDATE creature SET equipment_id=0 WHERE id=1341;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=119;
+UPDATE creature SET equipment_id=0 WHERE id=119;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=330;
+UPDATE creature SET equipment_id=0 WHERE id=330;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=244;
+UPDATE creature SET equipment_id=0 WHERE id=244;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=246;
+UPDATE creature SET equipment_id=0 WHERE id=246;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=894;
+UPDATE creature SET equipment_id=0 WHERE id=894;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=251;
+UPDATE creature SET equipment_id=0 WHERE id=251;
+
+UPDATE creature_template SET equipment_id=0 WHERE entry=247;
+UPDATE creature SET equipment_id=0 WHERE id=247;
