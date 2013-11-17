138609ab5ffb648cc18f8f247cafb39419d50ba9
Stalker <geany_100@yahoo.com>
2013-11-17 09:23:17 +0200

    Update vortex_pinnacle.cpp
    translate from german to english

--- src/server/scripts/Kalimdor/TheVortexPinnacle/vortex_pinnacle.cpp
+++ src/server/scripts/Kalimdor/TheVortexPinnacle/vortex_pinnacle.cpp
@@ -56,7 +56,7 @@ public:
         InstanceScript* instance = creature->GetInstanceScript();
 
         //if (instance && instance->GetData(DATA_GRAND_VIZIER_ERTAN)==DONE)
-            player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleportiere mich zur ersten Platform", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+1);
+            player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport me to next platform", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+1);
 
         player->SEND_GOSSIP_MENU(2475, creature->GetGUID());
         return true;
@@ -94,7 +94,7 @@ public:
         InstanceScript* instance = creature->GetInstanceScript();
 
         //if (instance && instance->GetData(DATA_ALTAIRUS)==DONE)
-            player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleportiere mich zur letzten Platform", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+1);
+            player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport me to next platform", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+1);
 
         player->SEND_GOSSIP_MENU(2475, creature->GetGUID());
         return true;
