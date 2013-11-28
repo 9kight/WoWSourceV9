dc8ee0044d865e25f32bdac432201964851a09c7
rnmkr <rnmkrmn@gmail.com>
2013-11-28 23:03:26 +0800

    Finished worgen zone 100%,  added some spawns and gobjects. Important thing was chain quest NPCs spawnmask was incorrect. So it was impossible to complete ^^
    And all available quest in Mount Hyjal is completable now.
    Due to Hyjal Portal doesn't work many available quests missing.

---
+++ sql/updates/world/creature_2013_11_28_02.sql
@@ -0,0 +1,2 @@
+UPDATE `creature` SET `spawnMask`='1032' WHERE `entry`=25574;
+UPDATE `creature` SET `phaseMask`='1032' WHERE `entry`=25574;
---
+++ sql/updates/world/gameobject_2013_11_28_01.sql
@@ -0,0 +1,14 @@
+DELETE FROM `gameobject` WHERE id=202702;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zone`, `area`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES ('4005830', '202702', '1', '616', '4979', '1', '1', '5240.77', '-2175.16', '1260.47', '0.613846', '0', '0', '0.302127', '0.953268', '300', '0', '1');
+
+DELETE FROM `gameobject` WHERE id=202703;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zone`, `area`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES ('4005831', '202703', '1', '616', '4979', '1', '1', '5244.46', '-2152.47', '1258.94', '6.2071', '0', '0', '0.038033', '-0.999276', '300', '0', '1');
+
+DELETE FROM `gameobject` WHERE id=202705;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zone`, `area`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES ('4005832', '202705', '1', '616', '4988', '1', '1', '5185.52', '-2129.95', '1277.34', '2.82683', '0', '0', '0.987641', '0.156732', '300', '0', '1');
+
+DELETE FROM `gameobject` WHERE id=202706;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zone`, `area`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES ('4005833', '202706', '1', '616', '4988', '1', '1', '5167.91', '-2134.51', '1277.84', '4.50599', '0', '0', '0.776188', '-0.630502', '300', '0', '1');
+
+DELETE FROM `gameobject` WHERE id=202697;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zone`, `area`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES ('4005834', '202697', '1', '616', '4988', '1', '1', '5200.41', '-2082.73', '1281.33', '4.64968', '0', '0', '0.728927', '-0.684591', '300', '0', '1');
