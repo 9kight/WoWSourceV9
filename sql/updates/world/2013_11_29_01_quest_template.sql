d81c45ec9c6c1c015ceeda5bff821f443198f570
rnmkr <rnmkrmn@gmail.com>
2013-11-29 22:16:22 +0800

    Oops, here it is.

---
+++ sql/updates/world/2013_11_29_01_quest_template.sql
@@ -0,0 +1,3 @@
+UPDATE `quest_template` SET `Method`='0' WHERE `entry`=12720;
+UPDATE `quest_template` SET `RequiredClasses`='0' WHERE `entry`=12720;
+UPDATE `quest_template` SET `RequiredRaces`='0' WHERE `entry`=12720;
