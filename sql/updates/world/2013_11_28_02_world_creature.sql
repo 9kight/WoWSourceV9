0c4beda036c10245877397b0c84575f30b533fb6
rnmkr <rnmkrmn@gmail.com>
2013-11-29 22:26:16 +0800

    Fix typo :3

--- sql/updates/world/2013_11_28_02_world_creature.sql
+++ sql/updates/world/2013_11_28_02_world_creature.sql
@@ -1,2 +1,2 @@
-UPDATE `creature` SET `spawnMask`='1032' WHERE `entry`=25574;
-UPDATE `creature` SET `phaseMask`='1032' WHERE `entry`=25574;
+UPDATE `creature` SET `spawnMask`='1032' WHERE `id`=25574;
+UPDATE `creature` SET `phaseMask`='1032' WHERE `id`=25574;
