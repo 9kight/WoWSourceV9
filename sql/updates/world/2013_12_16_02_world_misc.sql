a1392e501801d15ee344cca17142e018431ff2fd
rnmkr <rnmkrmn@gmail.com>
2013-12-16 00:53:05 +0800

    Fix typo

--- sql/updates/world/2013_12_16_02_world_misc.sql
+++ sql/updates/world/2013_12_16_02_world_misc.sql
@@ -1,34 +1,34 @@
-Entry: 12783
-LINK:http://www.wowhead.com/npc=12783
+-- Entry: 12783
+-- LINK:http://www.wowhead.com/npc=12783
 DELETE FROM creature WHERE guid=413494;
 INSERT INTO `creature` VALUES ('413494', '12783', '0', '1519', '5149', '1', '3969', '0', '12783', '-8746.41', '389.141', '101.055', '2.45181', '300', '0', '0', '5228', '0', '0', '0', '134250496', '0');
 
-ENTRY: 12805
-LINK:http://www.wowhead.com/npc=12805
+-- ENTRY: 12805
+-- LINK:http://www.wowhead.com/npc=12805
 DELETE FROM creature WHERE guid=413493;
 INSERT INTO `creature` VALUES (413493, 12805, 0, 1519, 5149, 1, 3969, 0, 12805, -8761.48, 389.984, 101.058, 0.598279, 300, 0, 0, 3529, 0, 0, 0, 0, 0);
 
-ENTRY: 52030
-LINK: http://www.wowhead.com/npc=52030
+-- ENTRY: 52030
+-- LINK: http://www.wowhead.com/npc=52030
 DELETE FROM creature WHERE guid=413492;
 INSERT INTO `creature` VALUES (413492, 52030, 0, 1519, 5149, 1, 3969, 0, 0, -8760.01, 411.142, 109.665, 3.91659, 300, 0, 0, 1830, 5013, 0, 0, 0, 0);
 
-ENTRY: 52029
-LINK: http://www.wowhead.com/npc=52029
+-- ENTRY: 52029
+-- LINK: http://www.wowhead.com/npc=52029
 DELETE FROM creature WHERE guid=52029;
 INSERT INTO `creature` VALUES ('413491', '52029', '0', '1519', '1519', '1', '3969', '0', '0', '-8771.62', '402.007', '109.665', '0.661114', '300', '0', '0', '2614', '0', '0', '0', '0', '0');
 
-Entry: 52027
-LINK: http://www.wowhead.com/npc=52027
+-- Entry: 52027
+-- LINK: http://www.wowhead.com/npc=52027
 DELETE FROM creature WHERE guid=320935;
 INSERT INTO `creature` VALUES (320935, 52027, 0, 1519, 1519, 1, 3969, 0, 0, -8802.82, 349.943, 108.814, 3.92056, 300, 0, 0, 1830, 5013, 0, 0, 0, 0);
 
-ENTRY: 58154
-LINK: http://www.wowhead.com/npc=58154
+-- ENTRY: 58154
+-- LINK: http://www.wowhead.com/npc=58154
 DELETE FROM creature WHERE guid=413488;
 INSERT INTO `creature` VALUES ('413488', '58154', '0', '1519', '1519', '1', '3969', '0', '0', '-8807.98', '350.587', '107.049', '3.72446', '300', '0', '0', '1830', '5013', '0', '0', '0', '0');
 
-ENTRY: 52028
-LINK: http://www.wowhead.com/npc=52028
+-- ENTRY: 52028
+-- LINK: http://www.wowhead.com/npc=52028
 DELETE FROM creature WHERE guid=52028;
 INSERT INTO `creature` VALUES ('413489', '52028', '0', '1519', '1519', '1', '3969', '0', '0', '-8822.84', '338.733', '107.049', '2.0869', '300', '0', '0', '1830', '5013', '0', '0', '0', '0');
