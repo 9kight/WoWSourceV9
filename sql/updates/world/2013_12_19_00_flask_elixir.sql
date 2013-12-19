b8416f9d8fe4d64eb6784d1eafb28bc3b710250a
Stalker <geany_100@yahoo.com>
2013-12-19 20:01:41 +0200

    Create 2013_12_19_00_flask_elixir.sql

---
+++ sql/updates/world/2013_12_19_00_flask_elixir.sql
@@ -0,0 +1,9 @@
+DELETE FROM `spell_script_names` WHERE ScriptName = "spell_item_flask_of_Enhancement";
+INSERT INTO `spell_script_names` VALUES
+(79637,"spell_item_flask_of_Enhancement");
+DELETE FROM `spell_script_names` WHERE ScriptName = "spell_item_Synapse_Springs";
+INSERT INTO `spell_script_names` VALUES
+(82174,"spell_item_Synapse_Springs");
+DELETE FROM `spell_script_names` WHERE ScriptName = "Grounded_Plasma_Shield";
+INSERT INTO `spell_script_names`  VALUES
+(82626,"Grounded_Plasma_Shield");
