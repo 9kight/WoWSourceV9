bd434b4ad661a1675b0463403bfa2d96b4b46fdd
Batbayar Bazarragchaa <digz6666@gmail.com>
2014-01-27 20:51:18 +0800

    1. Disable End Time and Zul'Gurub normal because there's no normal difficulty. 2. Fix typo in commit f1bd01895d3dde1fe9f0a696f47449fbbdfd2d5f 3. Remove duplicate Echo of Jaina, Echo of Sylvanas, Echo of Tyrande

--- sql/updates/world/2014_01_27_01_world_misc.sql
+++ sql/updates/world/2014_01_27_01_world_misc.sql
@@ -1,10 +1,10 @@
 -- duplicates
 DELETE FROM creature WHERE  guid IN (256235,256236,256234,256237,256220,256219,258582,256224,256222,256230,256225,256228,256227,256226,256233,256232,96976,127096);
 INSERT INTO `creature` VALUES (96976, 3174, 1, 14, 362, 1, 1, 0, 3174, 373.674, -4710.91, 15.9186, 1.38421, 300, 0, 0, 840, 0, 0, 0, 0, 0);
--- rong faction
+-- wrong faction
 UPDATE creature_template SET faction_A=7 AND faction_H=7 WHERE entry=37989;
 UPDATE creature_template SET faction_A=7 AND faction_H=7 WHERE entry=38002;
--- add  in the right place
+-- add in the right place
 DELETE FROM creature WHERE id=38002;
 INSERT INTO `creature` VALUES (NULL, 38002, 1, 14, 4875, 1, 1, 0, 0, -1305.14, -5456.09, 14.6958, 1.69075, 300, 0, 0, 42, 0, 0, 0, 0, 0);
 -- add the right emote
--- sql/updates/world/2014_01_27_04_creature.sql
+++ sql/updates/world/2014_01_27_04_creature.sql
@@ -1,3 +1,4 @@
 -- Duplicate boss remove (Jaina, Baine)
-DELETE FROM `creature` WHERE guid = '259228';
-DELETE FROM `creature` WHERE guid = '259224';
+DELETE FROM `creature` WHERE guid IN (259228, 259239); -- Echo of Tyrande
+DELETE FROM `creature` WHERE guid IN (259224, 259232); -- Echo of Echo of Sylvanas
+DELETE FROM `creature` WHERE guid IN (259225); -- Echo of Jaina
