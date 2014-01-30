fa525910e6301df23561e852c8fe7a367229d48b
Batbayar Bazarragchaa <digz6666@gmail.com>
2014-01-30 14:18:31 +0800

    Fix console log and a typo.

--- sql/updates/world/2014_01_29_02_spell_script.sql
+++ sql/updates/world/2014_01_29_02_spell_script.sql
@@ -1,5 +1,5 @@
 DELETE FROM `spell_script_names` WHERE `spell_id`=78; -- Warrior - Heroic Strike
-INSERT INTO `spell_script_names` VALUES (78, 'spell_script_names');
+INSERT INTO `spell_script_names` VALUES (78, 'spell_warr_heroic_strike');
 
 DELETE FROM `spell_bonus_data` WHERE `entry`=78; -- Warrior - Heroic Strike
 INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`, `ap_dot_bonus`, `comments`) VALUES (78, 0, 0, 0.6, 0, 'Warrior - Heroic Strike');
