e4ee066a76f1207117357cb6170a25cba6efc8ed
Batbayar Bazarragchaa <digz6666@gmail.com>
2014-02-04 13:57:51 +0800

    Fixed Starcatcher Compass, Seal of the Seven Signs, Soulshifter Vortex trinket procs. Fix spell_proc_event to commit: e41d0739e3be5ab46662b6123a0dea4a43476fbe

--- sql/updates/world/2014_02_01_04_spell_proc_event.sql
+++ sql/updates/world/2014_02_01_04_spell_proc_event.sql
@@ -1,2 +1,6 @@
-REPLACE INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES (107982, 0, 0, 0, 0, 0, 0, 0, 0, 15, 110);
-REPLACE INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES (109776, 0, 0, 0, 0, 0, 0, 0, 0, 15, 105);
+DELETE FROM `spell_proc_event` WHERE `entry`=107983; -- http://www.wowhead.com/item=77202 Starcatcher Compass
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES (107983, 0, 0, 0, 0, 0, 0, 0, 0, 15, 115);
+DELETE FROM `spell_proc_event` WHERE `entry`=107984; -- http://www.wowhead.com/item=77204 Seal of the Seven Signs
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES (107984, 0, 0, 0, 0, 0, 0, 0, 0, 15, 120);
+DELETE FROM `spell_proc_event` WHERE `entry`=109777; -- http://www.wowhead.com/item=77990 Soulshifter Vortex
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES (109777, 0, 0, 0, 0, 0, 0, 0, 0, 15, 105);
