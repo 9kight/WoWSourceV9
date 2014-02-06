22c29df295480fabbd83d727a293b5816b28d157
Stalker <geany_100@yahoo.com>
2014-02-06 09:43:27 +0200

    Create world_creature_loot_01.sql
    this are forum reports about missing  loot or rong loot

---
+++ sql/updates/world/world_creature_loot_01.sql
@@ -0,0 +1,59 @@
+-- normal loot for  http://www.wowhead.com/npc=39698
+DELETE FROM `creature_loot_template` WHERE entry='39698';
+INSERT INTO `creature_loot_template` VALUES ('39698', '55272', '12.1', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('39698', '55268', '12.03', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('39698', '55271', '11.83', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('39698', '55270', '8.24', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('39698', '55269', '7.85', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('39698', '71638', '1.33', '1', '0', '1', '1');
+UPDATE `creature_template` SET lootid='39698' WHERE entry='39698';
+
+-- heroic entry of 39698 its entry 39699
+DELETE FROM `creature_loot_template` WHERE entry='39699';
+INSERT INTO `creature_loot_template` VALUES ('39699', '56300', '8.63', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('39699', '56302', '8.37', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('39699', '56303', '8.23', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('39699', '56301', '8.18', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('39699', '56304', '8.14', '1', '0', '1', '1');
+UPDATE `creature_template` SET lootid='39699' WHERE entry='39699';
+
+-- normal loot for http://www.wowhead.com/npc=43878
+DELETE FROM `creature_loot_template` WHERE entry='43878';
+INSERT INTO `creature_loot_template` VALUES ('43878', '65660', '25.8', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('43878', '55834', '14.55', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('43878', '55833', '9.64', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('43878', '55830', '9.52', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('43878', '55831', '6.65', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('43878', '55832', '6.39', '1', '0', '1', '1');
+UPDATE `creature_template` SET lootid='43878' WHERE entry='43878';
+
+-- heroic entry of 43878 its entry 43879
+DELETE FROM `creature_loot_template` WHERE entry='43879';
+INSERT INTO `creature_loot_template` VALUES ('43879', '65660', '25.8', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('43879', '56358', '8.05', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('43879', '56360', '7.92', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('43879', '56357', '7.89', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('43879', '56359', '7.89', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('43879', '56356', '7.77', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('43879', '71715', '0.2', '1', '0', '1', '1');
+UPDATE `creature_template` SET lootid='43879' WHERE entry='43879';
+
+-- normal loot for http://www.wowhead.com/npc=43873
+DELETE FROM `creature_loot_template` WHERE entry='43873';
+INSERT INTO `creature_loot_template` VALUES ('43873', '55839', '10.65', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('43873', '55841', '10.55', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('43873', '55835', '10.38', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('43873', '55840', '10.34', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('43873', '55838', '6.85', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('43873', '63040', '0.78', '1', '0', '1', '1');
+UPDATE `creature_template` SET lootid='43873' WHERE entry='43873';
+
+-- heroic entry of 43873 its entry 43874
+DELETE FROM `creature_loot_template` WHERE entry='43874';
+INSERT INTO `creature_loot_template` VALUES ('43874', '56362', '9.01', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('43874', '56363', '8.88', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('43874', '56361', '8.84', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('43874', '56364', '8.75', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('43874', '56365', '8.72', '1', '0', '1', '1');
+INSERT INTO `creature_loot_template` VALUES ('43874', '63040', '0.78', '1', '0', '1', '1');
+UPDATE `creature_template` SET lootid='43874' WHERE entry='43874';
