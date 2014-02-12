7eaab93c058c0c2c7a273f60f622798fdf40ae47
VeraDev <laymasterlolz@gmail.com>
2014-02-12 12:41:05 +0100

    Templar's Verdict

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -11590,6 +11590,40 @@ bool Unit::isSpellCrit(Unit* victim, SpellInfo const* spellProto, SpellSchoolMas
                         }
                         break;
                     }
+					case SPELLFAMILY_PALADIN:
+                switch(spellProto->Id)
+                {
+                    // Templar's Verdict
+                    case 85256:
+                    {
+                        int32 damage = victim->CalculateDamage(BASE_ATTACK, true, true);
+
+                        // Divine Purpose check
+                        if (this->HasAura(90174))
+                        {
+                            damage *= 2.35f;
+                        }
+                        else
+                        {
+                            switch (this->GetPower(POWER_HOLY_POWER))
+                            {
+                                case 0: // 1 Holy Power
+                                    damage *= 0.3f;
+                                    break;
+                                case 1: // 2 Holy Power
+                                    damage *= 0.9f;
+                                    break;
+                                case 2: // 3 Holy Power
+                                    damage *= 2.35f;
+                                    break;
+                            }
+                        }
+
+						crit_chance += damage;
+                    }
+                    break;
+                }
+                break;
                     case SPELLFAMILY_WARRIOR:
                        // Victory Rush
                        if (spellProto->SpellFamilyFlags[1] & 0x100)
