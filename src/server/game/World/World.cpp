9kight/WoWSourceV9

commit 1ba0b1cdc1612c837d116fc97ee8c2a72ec4e939

Fix conquest point flush.

src/server/game/World/World.cpp
@@ -3014,8 +3014,7 @@ void World::ResetDailyQuests()
 void World::ResetCurrencyWeekCap()
 {
     CharacterDatabase.Execute("UPDATE `character_currency` SET `week_count` = 0");
-    CharacterDatabase.Execute("UPDATE `character_currency` SET `week_cap` = `new_cap`");
-    CharacterDatabase.Execute("UPDATE `character_currency` SET `new_cap` = 0");
+    CharacterDatabase.Execute("UPDATE `character_currency` SET `week_cap` = `0`");
 
     for (SessionMap::const_iterator itr = m_sessions.begin(); itr != m_sessions.end(); ++itr)
         if (itr->second->GetPlayer())

commit a0e23a1859b5a468d6fae42712f74c4912a5d262

Fix crash introduced in 1ba0b1cdc1612c837d116fc97ee8c2a72ec4e939

src/server/game/World/World.cpp
@@ -3014,7 +3014,7 @@ void World::ResetDailyQuests()
 void World::ResetCurrencyWeekCap()
 {
     CharacterDatabase.Execute("UPDATE `character_currency` SET `week_count` = 0");
-    CharacterDatabase.Execute("UPDATE `character_currency` SET `week_cap` = `0`");
+    CharacterDatabase.Execute("UPDATE `character_currency` SET `week_cap` = 0");
 
     for (SessionMap::const_iterator itr = m_sessions.begin(); itr != m_sessions.end(); ++itr)
         if (itr->second->GetPlayer())