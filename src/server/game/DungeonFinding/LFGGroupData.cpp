6d03a2c5b54ce4ca2ec5a6e0bc55ee31ade58a80
Stalker <geany_100@yahoo.com>
2014-04-18 19:29:07 +0300

    Update LFGGroupData.cpp

--- src/server/game/DungeonFinding/LFGGroupData.cpp
+++ src/server/game/DungeonFinding/LFGGroupData.cpp
@@ -1,19 +1,19 @@
 /*
- * Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+* Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
+*
+* This program is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the
+* Free Software Foundation; either version 2 of the License, or (at your
+* option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+* more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "LFG.h"
 #include "LFGGroupData.h"
@@ -37,13 +37,13 @@ void LfgGroupData::SetState(LfgState state)
 {
     switch (state)
     {
-        case LFG_STATE_NONE:
-        case LFG_STATE_DUNGEON:
-        case LFG_STATE_FINISHED_DUNGEON:
-            m_OldState = state;
-                    // No break on purpose
-        default:
-            m_State = state;
+    case LFG_STATE_NONE:
+    case LFG_STATE_DUNGEON:
+    case LFG_STATE_FINISHED_DUNGEON:
+        m_OldState = state;
+        // No break on purpose
+    default:
+        m_State = state;
     }
 }
 
@@ -62,7 +62,7 @@ void LfgGroupData::SetDungeon(uint32 dungeon)
 void LfgGroupData::DecreaseKicksLeft()
 {
     if (m_KicksLeft)
-      --m_KicksLeft;
+        --m_KicksLeft;
 }
 
 LfgState LfgGroupData::GetState() const
