858a0af3c69f37b890521619583a990cc31baf07
Stalker <geany_100@yahoo.com>
2014-04-19 18:23:44 +0300

    Update LFGGroupData.cpp

--- src/server/game/DungeonFinding/LFGGroupData.cpp
+++ src/server/game/DungeonFinding/LFGGroupData.cpp
@@ -1,26 +1,26 @@
 /*
-* Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
-*
-* This program is free software; you can redistribute it and/or modify it
-* under the terms of the GNU General Public License as published by the
-* Free Software Foundation; either version 2 of the License, or (at your
-* option) any later version.
-*
-* This program is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-* more details.
-*
-* You should have received a copy of the GNU General Public License along
-* with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "LFG.h"
 #include "LFGGroupData.h"
 
-
-LfgGroupData::LfgGroupData() : m_State(LFG_STATE_NONE), m_OldState(LFG_STATE_NONE),
-m_Leader(0), m_Dungeon(0), m_KicksLeft(LFG_GROUP_MAX_KICKS)
+LfgGroupData::LfgGroupData():
+m_State(LFG_STATE_NONE), m_OldState(LFG_STATE_NONE), m_Dungeon(0),
+m_VotesNeeded(LFG_GROUP_KICK_VOTES_NEEDED), m_KicksLeft(LFG_GROUP_MAX_KICKS)
 {
 }
 
@@ -28,26 +28,19 @@ LfgGroupData::~LfgGroupData()
 {
 }
 
-bool LfgGroupData::IsLfgGroup()
-{
-    return m_OldState != LFG_STATE_NONE;
-}
-
 void LfgGroupData::SetState(LfgState state)
 {
     switch (state)
     {
-    case LFG_STATE_NONE:
-    case LFG_STATE_DUNGEON:
-    case LFG_STATE_FINISHED_DUNGEON:
-        m_OldState = state;
-        // No break on purpose
-    default:
-        m_State = state;
+        case LFG_STATE_NONE:
+        case LFG_STATE_DUNGEON:
+        case LFG_STATE_FINISHED_DUNGEON:
+            m_OldState = state;
+                    // No break on purpose
+        default:
+            m_State = state;
     }
 }
-
-
 
 void LfgGroupData::RestoreState()
 {
@@ -62,7 +55,7 @@ void LfgGroupData::SetDungeon(uint32 dungeon)
 void LfgGroupData::DecreaseKicksLeft()
 {
     if (m_KicksLeft)
-        --m_KicksLeft;
+      --m_KicksLeft;
 }
 
 LfgState LfgGroupData::GetState() const
@@ -86,19 +79,4 @@ uint8 LfgGroupData::GetVotesNeeded() const
 uint8 LfgGroupData::GetKicksLeft() const
 {
     return m_KicksLeft;
-}
-
-uint8 LfgGroupData::GetPlayerCount() const
-{
-    return m_Players.size();
-}
-
-LfgState LfgGroupData::GetOldState() const
-{
-    return m_OldState;
-}
-
-LfgGuidSet const& LfgGroupData::GetPlayers() const
-{
-    return m_Players;
 }
