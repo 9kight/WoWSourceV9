ea1e11e8e5d6b33afe66885ae3e5ffe94c7665e5
Batbayar Bazarragchaa <digz6666@gmail.com>
2014-05-01 12:49:00 +0800

    Query fixes.

--- sql/updates/world/2014_04_26_01_world_misc.sql
+++ sql/updates/world/2014_04_26_01_world_misc.sql
@@ -614,7 +614,7 @@ INSERT INTO `spell_bonus_data` (`entry`, `direct_bonus`, `dot_bonus`, `ap_bonus`
 -- delete wrong npc loot
 DELETE FROM creature_loot_template WHERE entry=7168;
 
--- fixing rong equip for npc
+-- fixing wrong equip for npc
 -- http://www.wowhead.com/npc=708
-UPDATE creature_template SET equipment_id=0 WHERE entry=708; 
+--UPDATE creature_template SET equipment_id=0 WHERE entry=708;
 DELETE FROM creature_equip_template WHERE entry=708;
