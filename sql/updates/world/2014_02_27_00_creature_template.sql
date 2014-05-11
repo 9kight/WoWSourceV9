e90b438b86fa74aac362890c600f4efb88b16262
rnmkr <rnmkrmn@gmail.com>
2014-05-11 21:20:12 +0800

    Updating DB.

--- sql/updates/world/2014_02_27_00_creature_template.sql
+++
@@ -1,1 +0,0 @@
-UPDATE `creature_template` SET `ScriptName` = 'npc_tentacle_of_the_old_ones' WHERE `entry` IN (57220, 58077, 58078);
--- sql/updates/world/2014_03_05_03_shaman_tier12_2p.sql
+++
@@ -1,2 +0,0 @@
--- shaman resto tier 12 - 2 pair
-INSERT INTO `spell_proc_event` (`entry`, `SpellFamilyName`, `SpellFamilyMask2`, `procFlags`, `CustomChance`) VALUES (99190, 11, 16, 262144, 40);
