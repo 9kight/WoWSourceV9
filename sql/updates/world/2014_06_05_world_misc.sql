021f3349b2746f5710ac253ce8821ecea48ce79c
Stalker <geany_100@yahoo.com>
2014-06-05 17:53:56 +0300

    Update 2014_06_05_world_misc.sql
    fixed insertion errors

--- sql/updates/world/2014_06_05_world_misc.sql
+++ sql/updates/world/2014_06_05_world_misc.sql
@@ -1,12 +1,11 @@
--- Proc for copying the spells
 DELETE FROM spell_proc_event WHERE entry=49028;
 INSERT INTO `spell_proc_event` VALUES (49028, 0, 0, 0, 0, 0, 65552, 0, 0, 0, 0);
 
 -- Add parry buff
 DELETE FROM spell_linked_spell WHERE spell_trigger=49028;
-INSERT INTO `spell_linked_spell` VALUES (49028, 81256, 0, 'Dancing Rune Weapon parry application');
+INSERT INTO `spell_linked_spell` VALUES (49028, 81256, 0, 0, 'Dancing Rune Weapon parry application');
 
--- i will fix this when i get home
 -- Custom template of the npc
---DELETE FROM creature_template WHERE entry=27893;
---INSERT INTO `creature_template` VALUES (27893, 0, 0, 0, 0, 0, 1126, 11686, 0, 0, 'Rune Weapon', '', '', 0, 1, 1, 0, 0, 6, 6, 0, 1, 1.14286, 1, 0, 0, 0, 0, 24, 1, 3500, 0, 1, 0, 2048, 8, 0, 0, 0, 0, 1, 1, 0, 10, 1096, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 'npc_dancing_rune_weapon', 15595);
+DELETE FROM creature_template WHERE entry=27893;
+INSERT INTO `creature_template` VALUES ('27893', '0', '0', '0', '0', '0', '1126', '11686', '0', '0', 'Rune Weapon', '', '', '0', '85', '85', '3', '0', '6', '6', '0', '1', '1.14286', '1', '1', '1', '0', '215', '271', '0', '24', '1.7', '3500', '2000', '1', '0', '0', '8', '0', '0', '0', '0', '0', '215', '271', '24', '10', '1096', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', null, null, '', '0', '3', '1', '0.0905', '0', '1', '1', '0', '0', '0', '0', '0', '0', '0', '0', '1', '27893', '0', '0', 'npc_dancing_rune_weapon', '15595');
+
