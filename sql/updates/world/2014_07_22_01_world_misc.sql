647dda9c0ec149a759e7e91e238e400a0cc777c6
Stalker <geany_100@yahoo.com>
2014-07-22 12:36:44 +0300

    Update 2014_07_22_01_world_misc.sql

--- sql/updates/world/2014_07_22_01_world_misc.sql
+++ sql/updates/world/2014_07_22_01_world_misc.sql
@@ -1,5 +1,6 @@
--- missing spell declared
+-- missing spells declared
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (89267, 'spell_captain_cookie_setiated');
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (92834, 'spell_captain_cookie_setiated');
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (89732, 'spell_captain_cookie_nauseated');
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (92066, 'spell_captain_cookie_nauseated');
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (90681, 'spell_captain_cookie_throw_food_targeting');
