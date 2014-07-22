7f4ce1790c004b9244fd86d2f3a1fe2f987773ed
Stalker <geany_100@yahoo.com>
2014-07-22 12:56:28 +0300

    Update 2014_07_22_01_world_misc.sql

--- sql/updates/world/2014_07_22_01_world_misc.sql
+++ sql/updates/world/2014_07_22_01_world_misc.sql
@@ -4,3 +4,5 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (92834, 'spel
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (89732, 'spell_captain_cookie_nauseated');
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (92066, 'spell_captain_cookie_nauseated');
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (90681, 'spell_captain_cookie_throw_food_targeting');
+replace into `achievement_criteria_data` (`criteria_id`, `type`, `value1`, `value2`, `ScriptName`) values('6084','11','0','0','achievement_minutes_to_midnight');
+  
