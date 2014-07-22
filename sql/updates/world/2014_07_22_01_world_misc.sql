f8b55803b4f5a579f47982a3374fa22cd77abb7e
Stalker <geany_100@yahoo.com>
2014-07-22 12:27:40 +0300

    Create 2014_07_22_01_world_misc.sql
    missing spell declared

---
+++ sql/updates/world/2014_07_22_01_world_misc.sql
@@ -0,0 +1,5 @@
+-- missing spell declared
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (89267, 'spell_captain_cookie_setiated');
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (92834, 'spell_captain_cookie_setiated');
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (89732, 'spell_captain_cookie_nauseated');
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (92066, 'spell_captain_cookie_nauseated');
