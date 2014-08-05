495716cf8e308b42e0ba2b0e6be909047d34c87a
CATACLYSMDEV <catadev@yahoo.com>
2014-08-05 13:40:31 -0700

    LFG & LFR Fixed 100% By Jack

--- src/server/game/DungeonFinding/LFGPlayerData.h
+++ src/server/game/DungeonFinding/LFGPlayerData.h
@@ -1,25 +1,26 @@
 /*
- * Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2014 WoWSource 4.3.4
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
+ * Do Not Share The SourceCode
+ * and read our WoWSource Terms
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
+/* ScriptData
+SDName: LFG
+SD%Complete: 100%
+SDComment: Fully Working
+SDCategory: LFG
+EndScriptData
+*/
 
 #ifndef _LFGPLAYERDATA_H
 #define _LFGPLAYERDATA_H
 
-#include "Common.h"
 #include "LFG.h"
+
+namespace lfg
+{
 
 /**
     Stores all lfg data needed about the player.
@@ -32,32 +33,43 @@ class LfgPlayerData
 
         // General
         void SetState(LfgState state);
-        void ClearState();
-        void SetLockedDungeons(const LfgLockMap& lock);
+        void RestoreState();
+        void SetLockedDungeons(LfgLockMap const& lock);
+        void SetTeam(uint8 team);
+        void SetGroup(uint64 group);
+
         // Queue
         void SetRoles(uint8 roles);
-        void SetComment(const std::string& comment);
+        void SetComment(std::string const& comment);
         void SetSelectedDungeons(const LfgDungeonSet& dungeons);
-        void ClearSelectedDungeons();
 
         // General
         LfgState GetState() const;
-        const LfgLockMap& GetLockedDungeons() const;
+        LfgState GetOldState() const;
+        LfgLockMap const& GetLockedDungeons() const;
+        uint8 GetTeam() const;
+        uint64 GetGroup() const;
+
         // Queue
         uint8 GetRoles() const;
-        const std::string& GetComment() const;
-        const LfgDungeonSet& GetSelectedDungeons() const;
+        std::string const& GetComment() const;
+        LfgDungeonSet const& GetSelectedDungeons() const;
 
     private:
         // General
         LfgState m_State;                                  ///< State if group in LFG
-        LfgState m_OldState;                               ///< Old State
+        LfgState m_OldState;                               ///< Old State - Used to restore state after failed Rolecheck/Proposal
         // Player
         LfgLockMap m_LockedDungeons;                       ///< Dungeons player can't do and reason
+        uint8 m_Team;                                      ///< Player team - determines the queue to join
+        uint64 m_Group;                                    ///< Original group of player when joined LFG
+
         // Queue
         uint8 m_Roles;                                     ///< Roles the player selected when joined LFG
         std::string m_Comment;                             ///< Player comment used when joined LFG
         LfgDungeonSet m_SelectedDungeons;                  ///< Selected Dungeons when joined LFG
 };
+
+} // namespace lfg
 
 #endif
