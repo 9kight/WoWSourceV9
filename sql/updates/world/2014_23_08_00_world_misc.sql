4a246755cc3d3d7c70b8bbd6afaf3ed93cd22713
Loukie <loukie@ctwug.za.net>
2014-08-23 11:26:06 +0200

    DB/Log errror fix

--- sql/updates/world/2014_23_08_00_world_misc.sql
+++ sql/updates/world/2014_23_08_00_world_misc.sql
@@ -4,3 +4,9 @@ DELETE FROM achievement_criteria_data WHERE criteria_id = 6084 AND type = 11;
 
 -- these spells ids dnt exist at all
 DELETE FROM spell_script_names WHERE spell_id IN (-50294,-49145,-33851,13161,18541,34074);
+
+-- mising scriptname for boss deathwing
+UPDATE creature_template SET ScriptName = 'Teleportation_to_the_platforms' WHERE entry = 57328;
+
+-- myst if you change scriptnames make sure to make the changes in db to plz
+UPDATE creature_template SET ScriptName = 'npc_hungry_hobo' WHERE entry = 42617;
