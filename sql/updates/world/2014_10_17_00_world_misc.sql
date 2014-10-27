99bbef0b339600e8dab9bc7b3077a8ea63f95922
SQL Backup <help_pixbaz@yahoo.com>
2014-10-27 23:40:32 +0330

    Update 2014_10_17_00_world_misc.sql
    Duplicate And syntax error

--- sql/updates/world/2014_10_17_00_world_misc.sql
+++ sql/updates/world/2014_10_17_00_world_misc.sql
@@ -1,5 +1,6 @@
 -- db error Fix
 DELETE FROM creature_template_addon WHERE entry = 41146;
+DELETE FROM skill_fishing_base_level WHERE (entry=5140);
 INSERT INTO skill_fishing_base_level VALUES (5140,450);
 DELETE FROM creature_template_addon WHERE entry = 28017;
 
@@ -71,5 +72,5 @@ DELETE FROM `smart_scripts` WHERE `entryorguid`=@ENTRY AND `source_type`=0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
 (@ENTRY,0,0,0,9,0,100,0,0,5,7000,12000,11,14331,0,0,0,0,0,2,0,0,0,0,0,0,0,"Stonespine - In Combat - Cast 'Vicious Rend' (No Repeat) (Normal Dungeon)");
 
---chaof Orb Should be tradeable as patch of 4.3
+-- chaof Orb Should be tradeable as patch of 4.3
 UPDATE item_template SET bonding = 0 WHERE entry = 52078;
