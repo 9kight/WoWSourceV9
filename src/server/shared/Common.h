c22fc17b47bc710dc9e1bb1d4e6fcd06e0b735a5
ZeuL <bulbucan_liviu@yahoo.com>
2014-11-13 18:44:35 +0200

    Update Common.h
    forgot to del numbers...

--- src/server/shared/Common.h
+++ src/server/shared/Common.h
@@ -149,11 +149,11 @@ enum TimeConstants
 
 enum AccountTypes
 {
-152     SEC_PLAYER         = 0, 
-153     SEC_MODERATOR      = 1, 
-154     SEC_GAMEMASTER     = 2, 
-155     SEC_ADMINISTRATOR  = 3, 
-156     SEC_CONSOLE        = 4                                  // must be always last in list, accounts must have less security level always also
+     SEC_PLAYER         = 0, 
+     SEC_MODERATOR      = 1, 
+     SEC_GAMEMASTER     = 2, 
+     SEC_ADMINISTRATOR  = 3, 
+     SEC_CONSOLE        = 4                                  // must be always last in list, accounts must have less security level always also
 };
 
 enum LocaleConstant
