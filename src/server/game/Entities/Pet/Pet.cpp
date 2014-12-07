1ee7472686163c4c431da79a34224ff62612964e
vanasis <v.b_hellscream@yahoo.com>
2014-12-08 00:24:40 +0330

    Remove Old Dancing Rune Weapon

--- src/server/game/Entities/Pet/Pet.cpp
+++ src/server/game/Entities/Pet/Pet.cpp
@@ -981,12 +981,6 @@ bool Guardian::InitStatsForLevel(uint8 petlevel)
                     SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, maxDamage);
                     break;
                 }
-                case 27893:             // Dancing Rune Weapon
-                {
-                    SetRedirectThreat(100, m_owner->GetGUID());
-                    CastSpell(this, 51906, true);
-                    break;
-                }
                 case 89:                // Infernal
                 {
                     if (m_owner->GetTypeId() == TYPEID_PLAYER)
@@ -1611,7 +1605,7 @@ bool Pet::resetTalents()
             continue;
 
         TalentTabEntry const* talentTabInfo = sTalentTabStore.LookupEntry(talentInfo->TalentTab);
-
+27893
         if (!talentTabInfo)
             continue;
 
@@ -2139,11 +2133,6 @@ void Guardian::RecalculatePetScalingDamageDone()
     {
         auraId = 54566;
         effIndex = EFFECT_2;
-    }
-    else if (GetEntry() == 27893)
-    {
-        auraId = 51906;
-        effIndex = EFFECT_0;
     }
     else if (m_owner && m_owner->getClass() == CLASS_WARLOCK)
     {
